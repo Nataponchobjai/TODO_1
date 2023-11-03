53be5966a83d5709013b8a8ed011760921d6347f
Nataponchobjai <nataponchobjai@gmail.com>
2023-11-02 19:09:47 -0500

    2

--- main.js
+++ main.js
@@ -1,26 +1,17 @@
 let express = require("express");
-let PORT = 8004;
+let PORT = 8001;
 
 let app = express();
-
-let jsonParser = express.json();
-
 app.use(express.json()); 
 
 let routes = require("./routes");
 app.use(routes);
 
-
-
-
-
-
-
-
-app.get("/hello", function(req, res){
-    res.json("Hello there.");
-});
+// app.get("/hello", function(req, res){
+//     console.log("In GET /hello");
+//     resizeBy.json("Hello there.")
+//   });
 
 app.listen(PORT, function(){
-    console.log("Application started on port", PORT); 
-});
+    console.log("Application started on port", PORT);
+  });
--- routes.js
+++ routes.js
@@ -2,26 +2,30 @@ let express = require("express");
 
 let router = express.Router();
 
-let controllore = require("./controller");
-// add item
-router.post("/item", controllore.addItem);
-//get all item ( summary)
-router.get("/items", controllore.listItem);
-//get an item (details)
-router.get("/Item/:id", controllore.getItem);
-// delete an item
-router.delete("/item/:id", controllore.deleteItem);
-// update an item
-router.put("/item/:id", controllore.updateItem);
+let controller = require("./controller")
 
-// export the router object that has all the routes attached to it 
+// add item
+router.post("/items", controller.addItem)
+
+//get all items (summary)
+router.get("/items", controller.listItems)
+
+// get an item (details)
+router.get("/items/:id", controller.getItem)
+
+// delete an item
+router.delete("/items/:id", controller.deleteItem)
+
+// update an item
+router.put("/items/:id", controller.updateItem)
+
+
 module.exports = router;
 
 
-/**
- * POST - add an item
- * DELETE -  remove an item
- * PUT - update an item
- * GET - list the items (summary)
- * GET - get item (detail)
- */
+//**
+ // POST - add an item
+ // DELETE - remove an item
+ // PUT - update an item
+ // GET - list the items(summary)
+ // GET - get item (detail)
